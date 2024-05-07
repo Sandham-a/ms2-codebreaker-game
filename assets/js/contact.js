a52a91f57e8e67d86853bbfc9d942c7de4127622
Adam Sandham <z_a_sandham@hotmail.co.uk>
2024-05-07 23:27:18 +0100

    attempted to get contact form to contact email API with no luck

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,16 +2,21 @@
  * Function to that creates the variable of emailObj variable.
  * then sends the object to the email JS process the API request.
  */
-
-function sendEmail(){
-
-let emailObj = {
-  name: document.getElementById(form-name).value,
-  contactContent: document.getElementById(form-content).value, 
-  emailAddress: emailAddress,
-}
-
+function sendMail(contactForm) {
   let serviceID = "service_0fp6dju";
   let templateID = "template_k9hm21e";
-  emailjs.send(serviceID, templateID, emailObj)
-};
+  emailjs.send(serviceID, templateID, {
+      "from_name": contactForm.name.value,
+      "from_email": contactForm.form-email.value,
+      "message": contactForm.message.value
+  })
+  .then(
+      function(response) {
+          console.log("SUCCESS", response);
+      },
+      function(error) {
+          console.log("FAILED", error);
+      }
+  );
+  return false;  // To block from loading a new page
+}
